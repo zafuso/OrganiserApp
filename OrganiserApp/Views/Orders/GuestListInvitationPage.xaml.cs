5419d9990937afd21f5757e0f9ae93495cb053b4
zafuso <71095771+zafuso@users.noreply.github.com>
2022-06-06 12:05:26 +0200

    start on guestlist invitation layout

--- OrganiserApp/Views/Orders/GuestListInvitationPage.xaml.cs
+++ OrganiserApp/Views/Orders/GuestListInvitationPage.xaml.cs
@@ -15,4 +15,34 @@ public partial class GuestListInvitationPage : ContentPage
         base.OnAppearing();
 		(BindingContext as GuestListInvitationViewModel).Init();
     }
+
+    private void FirstName_Focused(object sender, FocusEventArgs e)
+    {
+        FirstName.BorderColor = Application.Current.Resources["Blue110"] as Color;
+    }
+
+    private void FirstName_Unfocused(object sender, FocusEventArgs e)
+    {
+        FirstName.BorderColor = Application.Current.Resources["Black10"] as Color;
+    }
+
+    private void LastName_Focused(object sender, FocusEventArgs e)
+    {
+        LastName.BorderColor = Application.Current.Resources["Blue110"] as Color;
+    }
+
+    private void LastName_Unfocused(object sender, FocusEventArgs e)
+    {
+        LastName.BorderColor = Application.Current.Resources["Black10"] as Color;
+    }
+
+    private void Note_Focused(object sender, FocusEventArgs e)
+    {
+        Note.BorderColor = Application.Current.Resources["Blue110"] as Color;
+    }
+
+    private void Note_Unfocused(object sender, FocusEventArgs e)
+    {
+        Note.BorderColor = Application.Current.Resources["Black10"] as Color;
+    }
 }
