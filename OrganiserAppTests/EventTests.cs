4e539a744f860212ac066e79ed212c24d6457ae9
zafuso <71095771+zafuso@users.noreply.github.com>
2022-06-14 16:37:36 +0200

    10 unit tests completed

---
+++ OrganiserAppTests/EventTests.cs
@@ -0,0 +1,30 @@
+﻿using OrganiserApp.Enums;
+using OrganiserApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganiserAppTests
+{
+    public class EventTests
+    {
+        private readonly EventService _sut = new();
+
+        [Fact]
+        public async Task GetEventsAsync_Test()
+        {
+            var result = await _sut.GetEventListAsync(FilterDateRangeType.all, 0, 5);
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task GetEventAsync_Test()
+        {
+            var result = await _sut.GetEventAsync("852ac0ee-ad49-42cb-9641-07631993b5bd");
+            Assert.NotNull(result);
+            Assert.Equal("Back to the '90s", result.Name);
+        }
+    }
+}
---
+++ OrganiserAppTests/ViewportTests.cs
@@ -0,0 +1,38 @@
+﻿using OrganiserApp.Models;
+using OrganiserApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganiserAppTests
+{
+    public class ViewportTests
+    {
+        private readonly ShopService _sut = new();
+        private readonly string EventUuid = "bc37c7da-1084-4caa-9426-f23156cb751f";
+
+        [Fact]
+        public async Task GetViewportsAsync_Test()
+        {
+            var result = await _sut.GetViewportsAsync(EventUuid);
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task PutViewportAsync_Test()
+        {
+            var newName = "goudennieuw";
+            var viewports = await _sut.GetViewportsAsync(EventUuid);
+
+            var testViewport = viewports.FirstOrDefault();
+            Assert.NotNull(testViewport);
+
+            testViewport!.Uri = newName;
+            var result = await _sut.PutViewportAsync(testViewport, EventUuid);
+
+            Assert.Equal(newName, result.Uri);
+        }
+    }
+}
